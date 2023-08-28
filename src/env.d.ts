e7cfa414a9fe46eee16520c1aefed35dd50ea215
karczujr <mateuszkarcz88@gmail.com>
2023-08-28 16:15:08 +0200

    Update env.d.ts 3

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,5 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-<<<<<<< HEAD
   readonly BUILDER_API_PUBLIC_KEY: string;
 }
-=======
-    readonly BUILDER_API_PUBLIC_KEY: string;
-  }
->>>>>>> master
